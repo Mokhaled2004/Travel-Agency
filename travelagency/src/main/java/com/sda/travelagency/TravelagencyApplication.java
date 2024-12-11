95552964bf52a92acd470b3677e2d1b0767e8cf3
Mokhaled2004 <mokhaled2004@gmail.com>
2024-12-11 20:33:15 +0200

    Env setup

---
+++ travelagency/src/main/java/com/sda/travelagency/TravelagencyApplication.java
@@ -0,0 +1,21 @@
+package com.sda.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+@SpringBootApplication
+public class TravelagencyApplication implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelagencyApplication.class, args);
+	}
+
+	@Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.setPort(8081); // Change 8081 to the desired port
+    }
+
+
+}
